c60f05ce6111449682260da052a6f327cd693c57
Weizhe Xing <weizhexing@Weizhes-MacBook-Pro.local>
2019-01-12 17:06:37 -0800

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,10 @@ const todos = [
     {   _id: new ObjectID(),
         text: 'First test todo'},
     {   _id: new ObjectID(),
-        text: 'Second test todo'}];
+        text: 'Second test todo',
+        completed: true,
+        completedAt : 333
+    }];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
@@ -104,6 +107,54 @@ describe('POST /todos', () => {
         });
     });
 
+    describe('PATCH /todos/:id', () => {
+        it('should update the todo', (done) => {
+            var hexId = todos[0]._id.toHexString();
+            var text = 'The new text';
+            request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(typeof res.body.todo.completedAt).toBe('number');
+            })
+            .end(done);
+        });
+
+        it('should clear completedAt when todo is not completed', (done) =>{
+            var hexId = todos[1]._id.toHexString();
+            var text = 'The new text';
+            request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
+            })
+            .end(done);
+        });
+
+        
+
+    });
+
+
+
+
+
+
+
+
     describe ('DELETE /todos/:id', () => {
         it('should delete a todo', (done) => {
             var hexId = todos[1]._id.toHexString();
@@ -138,5 +189,7 @@ describe('POST /todos', () => {
             .end(done);
         });
     });
+
+    
 
 });
