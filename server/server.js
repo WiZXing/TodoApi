9829db5caed5c38328ffeebd27efc81e449a7a96
Weizhe Xing <weizhexing@Weizhes-MacBook-Pro.local>
2019-01-12 16:29:25 -0800

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
-
+const _ = require('lodash');
 
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
@@ -86,7 +86,30 @@ app.delete('/todos/:id', (req, res) => {
             // 400 with empty body
 });
 
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+    if (_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo) => {
+        if (!todo)
+            return res.status(404).send();
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 
+
+
+});
 
 
 app.listen(port, () => {
